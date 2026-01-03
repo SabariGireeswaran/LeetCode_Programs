64e8d37dcd6fc44c87532542c6a8d23cfdc2bb2c
Sabari Gireeswaran B S R <bsrsabarigireeswaran@gmail.com>
2026-01-03 22:24:50 +0530

    fix:fixed the error in this file

--- lc_p3.py
+++ lc_p3.py
@@ -2,17 +2,19 @@ from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         length=len(height)
-        n=max(height)
-        for i in height:
-            if i==n:
-                line1=i
-                break
-        h=min(line1,height[length-1])
-        index1=height.index(line1)
-        index2=length-1
-        width=index2 - index1
-        area=h*width
-        return area
+        left = 0
+        right = length - 1
+
+        while left < right:
+            height = min(height[left], height[right])
+            width = right - left
+            area = height * width
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+            return area
+
     
 object1=Solution()
 print(object1.maxArea([1,2,1]))
