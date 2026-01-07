202c343e82b1bc10f6e29e9e252d5b772a5ee6e8
Sabari Gireeswaran B S R <bsrsabarigireeswaran@gmail.com>
2026-01-08 00:47:00 +0530

    feat:add palindrome problem solution

---
+++ lc_p6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        dup = x
+        n = x
+        rev = 0
+        while n > 0:
+            last_digit = n % 10
+            rev = (rev * 10) + last_digit
+            n = n // 10
+        if rev == dup:
+            if x < 0 and rev == dup:
+                return True
+            else:
+                return True
+        else:
+            return False
