7c28151cdadd1c620281d520d29810f9e012450f
Sabari Gireeswaran B S R <bsrsabarigireeswaran@gmail.com>
2026-01-18 20:49:39 +0530

    feat:add solution for the leetcode problem Longest Common Prefix

---
+++ lc_p7.py
@@ -0,0 +1,14 @@
+class LongestCommonPrefix:
+    def longest_common_prefix(self, strs):
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
+                
+
+
+
+
